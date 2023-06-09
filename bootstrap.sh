27fd3d9c7e92ab389c3fc6a54a6520256cba5dca
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2023-06-09 16:21:21 -0400

    add correct url

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
 echo 'Installing Homebrew';
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 
-echo 'Adding Homebrew to PAHT';
+echo 'Adding Homebrew to PATH';
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/mathiscote/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)";
 
+echo 'Bootstraping dependencies:';
 echo 'Installing Python';
 brew install python;
 
@@ -14,4 +15,4 @@ brew install wget
 echo 'Downloading python install script from Github'
 mkdir ~/temp/
 cd ~/temp/
-wget https://raw.githubusercontent.com/BenocxX/scripts/main/macos/install.py?token=GHSAT0AAAAAACDXERC6D3EJBRHZQ24E5GXIZEDRELQ
+wget https://raw.githubusercontent.com/BenocxX/macos/main/install.py
